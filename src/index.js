6d4c4305e41ec56a47f490e9433425b5d62cd978
mkebadirang9-art <mkebadirang9@gmail.com>
2025-09-29 21:56:53 +0200

    updated weather icon

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000)
+    let iconElement = document.querySelector("#icon")
+    
+    iconElement.innerHTML=`<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+
+
+
     
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML= Math.round(temperature);
 
+}
+function formatDate(date){
+   
+    let minutes = date.getMinutes
+    let hours = date.getHours
+    let days = ["Sunday" ,"Monday","Tuesday","Wednesday","Thursday","Friday","Sarturday",];
+    let day = days[date.getDay()];
+
+    if(minutes < 18){
+        minutes = `0${minutes}`;
+    }
+
+
+    return `${day} ${hours} ${minutes}`;
 }
 
 function searchCity(city){
@@ -26,4 +55,4 @@ function handleSearchSubmit(event){
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Paris");
+searchCity("Lisbon");
